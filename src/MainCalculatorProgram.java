9c3865a2bbf99a66a7641cbb94d619e15797e773
venkat <venkatdatla777@gmail.com>
2020-07-04 11:45:11 +0200

    Div Changed

--- src/MainCalculatorProgram.java
+++ src/MainCalculatorProgram.java
@@ -2,7 +2,7 @@ class MainCalculatorProgram {
     public static void main(String[] args) {
         int result;
         CalculatorMethods c = new CalculatorMethods();    //object created for calculatormethods page
-        result = c.Mul(563647, 20);            //calling the required method as we wish from calculator methods
+        result = c.Div(563647, 20);            //calling the required method as we wish from calculator methods
         System.out.println(result);
     }
 }
